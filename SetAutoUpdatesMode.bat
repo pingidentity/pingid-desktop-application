7c321c84b32a2fdabe2896181f621dce8741afe2
Asaf David <adavid@pingidentity.compingidentity.com>
2017-11-16 17:55:08 +0200

    rename files and update read

--- SetAutoUpdatesMode.bat
+++ SetAutoUpdatesMode.bat
@@ -1,5 +1,5 @@
 @echo off
-REM this script is suitable for PingID Desktop Application version 1.4
+REM this script is suitable for PingID Desktop Application version 1.5.0 and up
 set res="F"
 if "%1" == "Enable" set res="T"
 if "%1" == "enable" set res="T"
@@ -8,9 +8,9 @@ if "%1" == "disable" set res="T"
 if %res%=="T" (
 echo "Set Automatic Updates to %1"
 if "%PROCESSOR_ARCHITECTURE%" == "x86" (
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Prefs\com\pingidentity\pingid\pctoken\/P/R/O/D/U/C/T/I/O/N /v autoupdatemode /t REG_SZ /d %1
+"%PROGRAMFILES%\Ping Identity\PingID\AutoUpdatesSetup.exe" %*
 ) else (
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\JavaSoft\Prefs\com\pingidentity\pingid\pctoken\/P/R/O/D/U/C/T/I/O/N /v autoupdatemode /t REG_SZ /d %1
+"%PROGRAMFILES(x86)%\Ping Identity\PingID\AutoUpdatesSetup.exe" %*
 )) else (
 echo "Error: %1 is not a valid input, only Enable or Disable can be selected"
 )
---
+++ SetAutoUpdatesMode_before_v1.5.0.bat
@@ -0,0 +1,16 @@
+@echo off
+REM this script is suitable for PingID Desktop Application version 1.4
+set res="F"
+if "%1" == "Enable" set res="T"
+if "%1" == "enable" set res="T"
+if "%1" == "Disable" set res="T"
+if "%1" == "disable" set res="T"
+if %res%=="T" (
+echo "Set Automatic Updates to %1"
+if "%PROCESSOR_ARCHITECTURE%" == "x86" (
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Prefs\com\pingidentity\pingid\pctoken\/P/R/O/D/U/C/T/I/O/N /v autoupdatemode /t REG_SZ /d %1
+) else (
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\JavaSoft\Prefs\com\pingidentity\pingid\pctoken\/P/R/O/D/U/C/T/I/O/N /v autoupdatemode /t REG_SZ /d %1
+)) else (
+echo "Error: %1 is not a valid input, only Enable or Disable can be selected"
+)
