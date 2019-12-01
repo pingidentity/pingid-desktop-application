1c49f3768e1051ec76ac4398545fac4d3284ea3b
Taggert <4uxpux@mail.ru>
2019-12-01 17:13:00 +0200

    Update SetAutoUpdatesMode.bat
    PID-9033 PID-9035
    Small change in scripts of  autoupdate.
    Now they will work also on old versions of app and also on the new one

--- SetAutoUpdatesMode.bat
+++ SetAutoUpdatesMode.bat
@@ -8,9 +8,11 @@ if "%1" == "disable" set res="T"
 if %res%=="T" (
 echo "Set Automatic Updates to %1"
 if "%PROCESSOR_ARCHITECTURE%" == "x86" (
-"%PROGRAMFILES%\Ping Identity\PingID\AutoUpdatesSetup.exe" %*
+cd "%PROGRAMFILES%\Ping Identity\PingID\"
+AutoUpdatesSetup.exe %*
 ) else (
-"%PROGRAMFILES(x86)%\Ping Identity\PingID\AutoUpdatesSetup.exe" %*
+cd "%PROGRAMFILES(x86)%\Ping Identity\PingID\"
+AutoUpdatesSetup.exe %*
 )) else (
 echo "Error: %1 is not a valid input, only Enable or Disable can be selected"
 )
