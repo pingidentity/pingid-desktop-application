b7a28733d6e5467cb632cf6505aede371fc2ac62
Taggert <4uxpux@mail.ru>
2019-12-03 11:56:51 +0200

    Update SetAutoUpdatesMode.bat

--- SetAutoUpdatesMode.bat
+++ SetAutoUpdatesMode.bat
@@ -8,11 +8,13 @@ if "%1" == "disable" set res="T"
 if %res%=="T" (
 echo "Set Automatic Updates to %1"
 if "%PROCESSOR_ARCHITECTURE%" == "x86" (
-cd "%PROGRAMFILES%\Ping Identity\PingID\"
+pushd "%PROGRAMFILES%\Ping Identity\PingID\"
 AutoUpdatesSetup.exe %*
+popd
 ) else (
-cd "%PROGRAMFILES(x86)%\Ping Identity\PingID\"
+pushd "%PROGRAMFILES(x86)%\Ping Identity\PingID\"
 AutoUpdatesSetup.exe %*
+popd
 )) else (
 echo "Error: %1 is not a valid input, only Enable or Disable can be selected"
 )
